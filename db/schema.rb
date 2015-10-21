7b7805cd09778f3e2db83599973f093ab9bc3568
Vivek Poola <vpoola88@gmail.com>
2015-10-20 22:35:59 -0700

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,11 @@ ActiveRecord::Schema.define(version: 20151021043142) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "recipes_users", id: false, force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
