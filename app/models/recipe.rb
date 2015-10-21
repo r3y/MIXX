cac728457138d80e82e9ebd818c2a52ef4b7fb77
Vivek Poola <vpoola88@gmail.com>
2015-10-20 21:43:50 -0700

    add favorite_recipes association and join table for users and recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,10 @@ class Recipe < ActiveRecord::Base
   has_many :categories, :through => :category_recipes
   
   belongs_to :user
-  has_many   :favorites
+
+  has_many   :favorite_recipes
+  has_many :favorited_by, through: :favorite_recipes, source: :user
+
+
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  has_many :favorites
+  has_many :recipes
+
+  has_many :favorite_recipes
+  has_many :favorites, through: :favorite_recipes, source: :recipe
 end
